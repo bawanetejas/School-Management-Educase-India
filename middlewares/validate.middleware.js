ef4b707f4c1b91f3913db97f4f430cd902f25664
bawanetejas <tejasbawane221@gmail.com>
2026-05-22 11:33:53 +0530

    refactor:simplyfy validation middleware

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -2,6 +2,7 @@
 
 const validate = (schema, source = 'body') => {
     return (req, res, next) => {
+
         const result = schema.safeParse(req[source]);
 
         if (!result.success) {
@@ -9,10 +10,7 @@ const validate = (schema, source = 'body') => {
                 success: false,
                 message: 'Validation failed',
 
-                errors: result.error.errors.map(err => ({
-                    field: err.path.join('.'),
-                    message: err.message
-                }))
+                errors: result.error.errors
             });
         }
 
